5079d9519fbc3ccf5d88e6c70c285101a9c045d7
Ariel Zhen <zhenpingping@live.com>
2018-07-27 00:07:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,9 +9,14 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
-
           resp.write "#{item.price}"
-    elsif
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"  
     end
     resp.finish
   end
