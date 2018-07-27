4d7c2eb219b4345bf5dcb983ff9c7d61a6f09339
Ariel Zhen <zhenpingping@live.com>
2018-07-27 00:05:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,11 +5,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
-    if req.path.match(/item/)
-      @@items.each do |item|
-      resp.write "#{item.price}"
-      end
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if item = @@items.find{|i| i.name == item_name}
+
+          resp.write "#{item.price}"
     elsif
     end
     resp.finish
